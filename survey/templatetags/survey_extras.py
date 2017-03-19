aa3d3d4d978075cdf03f1023d46228ac77161f93
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2017-03-19 14:00:04 +0100

    Feat - Made the form collapsible when there is no validation errors

--- survey/templatetags/survey_extras.py
+++ survey/templatetags/survey_extras.py
@@ -5,6 +5,21 @@ from django import template
 register = template.Library()
 
 
+def collapse_form(form, category):
+    """ Permit to return the class of the collapsible  according to errors in
+    the form. """
+    categories_with_error = set()
+    for field in form:
+        if field.errors:
+            categories_with_error.add(field.field.widget.attrs["category"])
+    if category.name in categories_with_error:
+        return "in"
+    else:
+        return ""
+
+register.filter('collapse_form', collapse_form)
+
+
 class CounterNode(template.Node):
 
     def __init__(self):
