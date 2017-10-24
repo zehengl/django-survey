fa754a713736dc710cab02e02e8f0ec24ba95bb8
Pierre Sassoulas <pierre.sassoulas@cea.fr>
2017-10-24 12:36:33 +0200

    Fix - Made the Exception compatible with python 2 and 3
    See : https://stackoverflow.com/questions/46076279/

--- survey/management/survey_command.py
+++ survey/management/survey_command.py
@@ -5,6 +5,7 @@ from __future__ import (
 )
 
 from builtins import str
+from sys import version_info
 
 from django.core.management.base import BaseCommand
 from future import standard_library
@@ -27,7 +28,16 @@ class SurveyCommand(BaseCommand):
             help='The primary key of the survey we want to generate.'
         )
 
-    def clean_error_message(self, value):
+    def raise_value_error(self, value):
+        msg = self.clean_error_message_survey(value)
+        # Compatibility for python 2/3
+        # See: https://stackoverflow.com/questions/46076279/
+        if version_info.major == 2:
+            raise ValueError(msg.encode('utf-8'))
+        else:
+            raise ValueError(msg)
+
+    def clean_error_message_survey(self, value):
         """ Return a clean error message if an argument is wrong.
 
         :param string value: the attempted value. """
@@ -39,17 +49,16 @@ class SurveyCommand(BaseCommand):
         return msg
 
     def handle(self, *args, **options):
+        self.survey = None
         if options['survey_name']:
             survey_name = options['survey_name'][0]
             try:
                 self.survey = Survey.objects.get(name=survey_name)
             except Survey.DoesNotExist:
-                raise ValueError(self.clean_error_message(survey_name))
+                self.raise_value_error(survey_name)
         elif options['survey_id']:
             survey_id = options['survey_id'][0]
             try:
                 self.survey = Survey.objects.get(pk=survey_id)
             except Survey.DoesNotExist:
-                raise ValueError(self.clean_error_message(survey_id))
-        else:
-            self.survey = None
+                self.raise_value_error(survey_id)
