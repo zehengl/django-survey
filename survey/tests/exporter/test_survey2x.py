9b449086275ec61c9b6969df4c2f725b442133c8
Thomas Milliman <thomas.milliman@unh.edu>
2018-02-13 13:26:01 -0500

    added some comments and a sleep() to deal with file system delays which were causing test to fail

--- survey/tests/exporter/test_survey2x.py
+++ survey/tests/exporter/test_survey2x.py
@@ -6,6 +6,7 @@ from __future__ import (
 
 import os
 import time
+from datetime import datetime
 
 from django.conf import settings
 from future import standard_library
@@ -35,10 +36,39 @@ class TestSurvey2X(TestManagement):
         expected = os.path.join(settings.ROOT, "survey",
                                 "test-management-survey.survey")
         self.assertEqual(s2xi.file_name(), expected)
+
+        # survey output file shouldn't exist but responses are
+        # already created
+        responses = s2xi.survey.responses.all()
+        print("XXXXXXX number of responses: {}".format(len(responses)))
+        lad = s2xi.survey.latest_answer_date()
+        print("XXXXXXX lad before file creation: {}".format(lad))
         self.assertTrue(s2xi.need_update())
+        time.sleep(1)
+
+        # write dummy survey file
         s2xi.survey_to_x()
+        mtime = os.path.getmtime(expected)
+        fctime = datetime.fromtimestamp(mtime)
+        fcztime = fctime.replace(tzinfo=lad.tzinfo)
+        print("XXXXXXX mtime: {}".format(fcztime))
+        
+        # no responses saved Survey2X created need_update and
+        # should return False
+        lad = s2xi.survey.latest_answer_date()
+        print("XXXXXXX lad after file creation: {}".format(lad))
         self.assertFalse(s2xi.need_update())
+
+        # save response
         self.response.save()
+
+        # now need_update should return True
+        lad = s2xi.survey.latest_answer_date()
+        print("XXXXXXX lad after save(): {}".format(lad))
         self.assertTrue(s2xi.need_update())
+
+        # remove survey file
         if os.path.exists(expected):
             os.remove(expected)
+        else:
+            print("Error removing file")
