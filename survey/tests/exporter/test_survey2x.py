0b449d944a9fb0e34925c1af8ca34065b46c9d9d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-02-16 22:18:26 +0100

    Style - Logging instead of print in tmilliman fix

--- survey/tests/exporter/test_survey2x.py
+++ survey/tests/exporter/test_survey2x.py
@@ -4,16 +4,21 @@ from __future__ import (
     absolute_import, division, print_function, unicode_literals
 )
 
+from datetime import datetime
+import logging
 import os
 import time
-from datetime import datetime
 
 from django.conf import settings
 from future import standard_library
+
 from survey.exporter.survey2x import Survey2X
 from survey.tests.management.test_management import TestManagement
 
+
 standard_library.install_aliases()
+
+LOGGER = logging.getLogger(__name__)
 
 
 class TestSurvey2X(TestManagement):
@@ -40,9 +45,9 @@ class TestSurvey2X(TestManagement):
         # survey output file shouldn't exist but responses are
         # already created
         responses = s2xi.survey.responses.all()
-        print("XXXXXXX number of responses: {}".format(len(responses)))
+        LOGGER.debug("Number of responses: %s", len(responses))
         lad = s2xi.survey.latest_answer_date()
-        print("XXXXXXX lad before file creation: {}".format(lad))
+        LOGGER.debug("latest_answer_date before file creation: %s", lad)
         self.assertTrue(s2xi.need_update())
         time.sleep(1)
 
@@ -51,12 +56,12 @@ class TestSurvey2X(TestManagement):
         mtime = os.path.getmtime(expected)
         fctime = datetime.fromtimestamp(mtime)
         fcztime = fctime.replace(tzinfo=lad.tzinfo)
-        print("XXXXXXX mtime: {}".format(fcztime))
-        
+        LOGGER.debug("mtime: %s", fcztime)
+
         # no responses saved Survey2X created need_update and
         # should return False
         lad = s2xi.survey.latest_answer_date()
-        print("XXXXXXX lad after file creation: {}".format(lad))
+        LOGGER.debug("latest_answer_date after file creation: %s", lad)
         self.assertFalse(s2xi.need_update())
 
         # save response
@@ -64,11 +69,11 @@ class TestSurvey2X(TestManagement):
 
         # now need_update should return True
         lad = s2xi.survey.latest_answer_date()
-        print("XXXXXXX lad after save(): {}".format(lad))
+        LOGGER.debug("latest_answer_date after save(): %s", lad)
         self.assertTrue(s2xi.need_update())
 
         # remove survey file
         if os.path.exists(expected):
             os.remove(expected)
         else:
-            print("Error removing file")
+            LOGGER.error("Error removing '%s'", expected)
