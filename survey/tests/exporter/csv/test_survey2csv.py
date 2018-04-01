7cbb0bd5387f64a782be866272cbe05e0eb66a44
Pierre Sassoulas <pierre.sassoulas@wisebim.fr>
2018-04-01 13:27:50 +0200

    Fix - Path on non linux based system can't contain /

--- survey/tests/exporter/csv/test_survey2csv.py
+++ survey/tests/exporter/csv/test_survey2csv.py
@@ -49,4 +49,5 @@ class TestSurvey2Csv(TestManagement):
     def test_filename(self):
         """ Filename is not an unicode object or os.path and others fail. """
         name = self.s2csv.file_name()
-        self.assertIn("csv/test-management-survey.csv", name)
+        self.assertIn("csv", name)
+        self.assertIn("test-management-survey.csv", name)
