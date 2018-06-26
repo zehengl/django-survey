965a44fe6dd72e4ebc895808e5a7549861fa329d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-06-26 21:59:55 +0200

    Test - Use patch so we do not generate a file for real
    Permit to not have concurrency problem with the date being really
    close when testing if we need to update. This has been a problem
    for far too long.

--- survey/tests/exporter/test_survey2x.py
+++ survey/tests/exporter/test_survey2x.py
@@ -12,6 +12,7 @@ from datetime import datetime
 import pytz
 from django.conf import settings
 from future import standard_library
+from mock.mock import patch
 
 from survey.exporter.survey2x import Survey2X
 from survey.tests.management.test_management import TestManagement
@@ -20,11 +21,17 @@ standard_library.install_aliases()
 
 LOGGER = logging.getLogger(__name__)
 
+
 class Survey2Survey(Survey2X):
+
     def survey_to_x(self):
-        file_ = open(self.file_name(), "w")
-        file_.write(".")
-        file_.close()
+        pass
+
+
+LONG_TIME_AGO = datetime(1990, 1, 1, 0, 0, 0)
+SHORT_TIME_AGO = datetime(2000, 1, 1, 0, 0, 0)
+RIGHT_NOW = datetime(2010, 1, 1, 0, 0, 0)
+
 
 class TestSurvey2X(TestManagement):
 
@@ -36,8 +43,8 @@ class TestSurvey2X(TestManagement):
         self.expected_virtual = os.path.join(settings.ROOT, "x", "test-management-survey.x")
 
     def get_fail_info(self, survey2x):
-        msg = "Latest answer date : {} ".format(survey2x.latest_answer_date)
-        msg += "File modification time : {}".format(survey2x.file_modification_time)
+        msg = "\nLatest answer date :     {}".format(survey2x.latest_answer_date)
+        msg += "\nFile modification time : {}".format(survey2x.file_modification_time)
         return msg
 
     def test_survey_2_x(self):
@@ -47,22 +54,21 @@ class TestSurvey2X(TestManagement):
         self.assertEqual(self.actual_survey2x.file_name(), self.expected_actual)
         self.assertEqual(self.virtual_survey2x.file_name(), self.expected_virtual)
 
-    def test_need_update(self):
+    def test_initially_need_update(self):
         self.assertTrue(self.actual_survey2x.need_update(), "No file exported and the survey "
-                        "contain response : we should need an update."
+                        "contain response : we should need an update. "
                         "{}".format(self.get_fail_info(self.actual_survey2x)))
-        self.actual_survey2x.survey_to_x()
-        time.sleep(1)
+
+    @patch.object(Survey2Survey, "file_modification_time", RIGHT_NOW)
+    @patch.object(Survey2Survey, "latest_answer_date", LONG_TIME_AGO)
+    def test_need_update_after_generation(self):
         self.assertFalse(self.actual_survey2x.need_update(), "We exported the file and there"
-                         " is no new response, we should not need an update."
+                         " is no new response, we should not need an update. "
                          "{}".format(self.get_fail_info(self.actual_survey2x)))
-        time.sleep(1)
-        self.response.save()
-        lad = self.actual_survey2x.survey.latest_answer_date()
+
+    @patch.object(Survey2Survey, "file_modification_time", LONG_TIME_AGO)
+    @patch.object(Survey2Survey, "latest_answer_date", RIGHT_NOW)
+    def test_need_update_after_new_response(self):
         self.assertTrue(self.actual_survey2x.need_update(), "We exported the file but there"
-                         " is a new response, we should need an update."
-                         "{}".format(self.get_fail_info(self.actual_survey2x)))
-        if os.path.exists(self.expected_actual):
-            os.remove(self.expected_actual)
-        else:
-            LOGGER.error("Error removing '%s'", self.expected_actual)
+                        " is a new response, we should need an update. "
+                        "{}".format(self.get_fail_info(self.actual_survey2x)))
