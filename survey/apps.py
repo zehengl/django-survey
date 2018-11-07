b75a78db80ba67a1687d6cceef7f37ded41c75ef
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2018-11-07 08:42:40 +0100

    Doc - Add a docstring in survey/apps.py

--- survey/apps.py
+++ survey/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
 
-class RockNRollConfig(AppConfig):
+
+class DjangoSurveyAndReportConfig(AppConfig):
+    
+    """
+    See https://docs.djangoproject.com/en/2.1/ref/applications/#django.apps.AppConfig
+    """
+    
     name = 'survey'
-    verbose_name = "Survey and report"
+    label = 'survey'
+    verbose_name = 'Survey and report'
