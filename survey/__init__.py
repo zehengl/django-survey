b133bb01096df7ab40bc301aed5b3d6a72f3b75e
Daniel Bautista <danielbautista@Administrators-MacBook-Pro.local>
2019-07-26 12:49:06 -0600

    Fix - 'Settings' object missing attribute (#41)

---
+++ survey/__init__.py
@@ -0,0 +1,14 @@
+def set_default_settings():
+    try:
+        from django.conf import settings
+        from . import settings as app_settings
+
+        for setting in dir(app_settings):
+            if setting in ["CHOICES_SEPARATOR", "ROOT"]:
+                if not hasattr(settings, setting):
+                    setattr(settings, setting, getattr(app_settings, setting))
+    except ImportError:
+        pass
+
+
+set_default_settings()
