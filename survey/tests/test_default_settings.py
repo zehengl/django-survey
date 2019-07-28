d9b46a4d06bf6832aa5dbb394ae97325e0578400
guam68 <guam_68@hotmail.com>
2019-07-27 20:36:10 -0600

    Add - Test for setting ROOT

--- survey/tests/test_default_settings.py
+++ survey/tests/test_default_settings.py
@@ -1,19 +1,26 @@
 from survey.tests import BaseTest
 from django.test import override_settings
 from django.conf import settings
-from django.test import tag
 from survey import set_default_settings
+from survey.exporter.tex.survey2tex import Survey2Tex
 
 
-@tag("set")
 @override_settings()
 class TestDefaultSettings(BaseTest):
     def test_set_choices_separator(self):
         url = "/admin/survey/survey/1/change/"
         del settings.CHOICES_SEPARATOR
         self.login()
-        with self.assertRaises(AttributeError):
-            self.client.get(url)
         set_default_settings()
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        try:
+            self.client.get(url)
+        except AttributeError:
+            self.fail("AttributeError: survey failed to set CHOICES_SEPARATOR")
+
+    def test_set_root(self):
+        del settings.ROOT
+        set_default_settings()
+        try:
+            Survey2Tex.generate(self, "/")
+        except AttributeError:
+            self.fail("AttributeError: survey failed to set ROOT")
