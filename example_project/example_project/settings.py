a68770a785b96e5c89abc72d0c71416f3dc1db30
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-02-09 21:12:33 +0100

    Easier testing in the example project

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# CSV_DIRECTORY = Path(__file__).parent.parent.joinpath("csv")
+# TEX_DIRECTORY = Path(__file__).parent.parent.joinpath("tex")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
