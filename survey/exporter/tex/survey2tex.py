7a17ecda8a3577b4ba436a3f40b9687d0071c5e0
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-02-23 09:53:38 +0100

    Use subprocess instead of os.system for LaTeX generation

--- survey/exporter/tex/survey2tex.py
+++ survey/exporter/tex/survey2tex.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+import subprocess
 from datetime import datetime
 from pathlib import Path
 from pydoc import locate
@@ -122,10 +123,10 @@ class Survey2Tex(Survey2X):
             LOGGER.debug("Moving <%s> to <%s> (will delete <%s> later)", dep, dir_name, dependency)
             copy(dep, dir_name)
             dependencies_to_delete.append(dependency)
-        xelatex_command = "xelatex -interaction=batchmode -halt-on-error {}".format(file_name)
-        os.system(xelatex_command)
+        xelatex_command = ["xelatex", "-interaction=batchmode", "-halt-on-error", file_name]
+        subprocess.check_call(xelatex_command)
         # Table of content and reference need two compilations for the link to be correct
-        os.system(xelatex_command)
+        subprocess.check_call(xelatex_command)
         if output is not None:
             os.system("mv {}.pdf {}".format(file_name[:-3], output))
         for dep in dependencies_to_delete:
