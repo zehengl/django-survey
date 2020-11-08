ee3c3518beef09d0eb412dfc1da9829c8e888e63
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-11-08 10:19:13 +0100

    Add .mo file for brazilian portuguese and indonesian
    Closes #106

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ DEV_DEPENDENCIES = [
 
 setuptools.setup(
     name="django-survey-and-report",
-    version="1.3.27",
+    version="1.3.28",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
