1ffa2110813bdbe8b878b70348bfcc4db4915b27
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2020-11-24 21:46:15 +0100

    Upgrade to 1.3.31

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ DEV_DEPENDENCIES = [
 
 setuptools.setup(
     name="django-survey-and-report",
-    version="1.3.30",
+    version="1.3.31",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
