474f44d9696cc3bddc123d4de4e2d44c0b48a4ae
anthonyrandell <anthony.randell@gmail.com>
2021-08-12 13:11:47 +0000

    Made image template a class attribute rather than local (#148)
    * Made image template a class attribute rather than local
    
    Co-authored-by: Anthony Randell <anthony.randell@gov.wales>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- survey/widgets.py
+++ survey/widgets.py
@@ -3,6 +3,8 @@ from django.template.loader import render_to_string
 
 
 class ImageSelectWidget(forms.widgets.Widget):
+    template_name = "survey/forms/image_select.html"
+
     class Media:
         js = (
             "http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js",
@@ -11,12 +13,11 @@ class ImageSelectWidget(forms.widgets.Widget):
         )
 
     def render(self, name, value, *args, **kwargs):
-        template_name = "survey/forms/image_select.html"
         choices = []
         for index, choice in enumerate(self.choices):
             if choice[0] != "":
                 value, img_src = choice[0].split(":", 1)
                 choices.append({"img_src": img_src, "value": value, "full_value": choice[0], "index": index})
         context = {"name": name, "choices": choices}
-        html = render_to_string(template_name, context)
+        html = render_to_string(self.template_name, context)
         return html
