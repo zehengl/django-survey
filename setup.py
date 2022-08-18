7da505d23e124b006dc41d8b0f67e139e6d7377d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-08-18 09:20:04 +0200

    Drop python 3.6
    We're not ready for python 3.11 yet

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
